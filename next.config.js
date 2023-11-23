b7356d2b2c9b9d292e4c615aca4282546646dc50
Xuân Hùng <xuanhung482k@gmail.com>
2023-11-23 16:28:16 +0700

    Setup Contentlayer

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
+
+const { withContentlayer } = require('next-contentlayer');
+
 const nextConfig = {};
 
-module.exports = nextConfig;
+module.exports = withContentlayer({ ...nextConfig });
