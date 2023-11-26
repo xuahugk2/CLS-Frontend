2222e0cb03eeef78160d4ce76ff66e92bbae46f0
Xuân Hùng <xuanhung482k@gmail.com>
2023-11-26 17:14:51 +0700

    Fix: Remove Console and others

--- src/components/About/AboutCoverSection.tsx
+++ src/components/About/AboutCoverSection.tsx
@@ -8,7 +8,7 @@ const AboutCoverSection = () => {
       <div className="w-full md:w-1/2 h-full border-r-2 border-solid border-dark dark:border-light flex justify-center">
         <Image
           src={profileCharacter}
-          alt="CodeBucks"
+          alt="CLS"
           className="w-4/5 xs:w-3/4 md:w-full h-full object-contain object-center"
           priority
           sizes="(max-width: 768px) 100vw,(max-width: 1180px) 50vw, 50vw"
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -65,7 +65,7 @@ const Footer = () => {
 
       <div className="w-full  mt-16 md:mt-24 relative font-medium border-t border-solid border-light py-6 px-8 flex  flex-col md:flex-row items-center justify-between">
         <span className="text-center">
-          &copy;2023 CodeBucks. All rights reserved.
+          &copy;2023 CLS Dev Team. All rights reserved.
         </span>
         <Link
           href="/sitemap.xml"
